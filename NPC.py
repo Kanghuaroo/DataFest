5b4f266d474c41118abfa0d8b32ea720d1aa1522
David Miller <5035dcmiller@gmail.com>
2022-04-09 14:34:17 -0400

    added Invite class and more functionality

--- NPC.py
+++ NPC.py
@@ -6,7 +6,31 @@ class NPC():
 
         self.connections = list()
         self.rank = "Not Sure"
+    
+    def getData(self):
+        output = list()
+        vals = ['data0Text', 
+                'data1Text', 
+                'data2text', 
+                'data3Text']
+
+        for i in vals:
+            if self.args.get(i):
+                if type(self.args.get(i)) == type('str'):
+                    output.append(self.args.get(i))
+        
+        return output
 
     def addConnections(self, person_id):
         self.connections.append(person_id)
 
+class Invitation():
+    def __init__(self, args):
+        self.unique_id = args.get('id')
+        self.args = args
+
+    def getInvolved(self):
+        return self.args.get('peopleInvolved')
+    
+    def getInvite(self):
+        return self.args.get("inviteText")
